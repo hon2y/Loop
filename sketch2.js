fbe6dfcb656a823b75819253a2db60cd86470ec8
hon2y <hon222y@gmail.com>
2014-09-11 16:08:28 +0900

    ttt

--- sketch2.js
+++ sketch2.js
@@ -1,11 +1,29 @@
 function setup() {
   createCanvas(640, 480);
-  
+  background(128, 0, 65);
   noFill();
 
   stroke(255, 0, 0);
 
+/*
+  var x;
+
+  x = 100;
+  ellipse(x, 100, 50, 50);
+
+  x = 250;
+  ellipse(x, 100, 50, 50);
+
+  x = 300;
+  ellipse(x, 100, 50, 50);
+
+
+for (var x = 1; x <=10; x++)
+{
+  ellipse(x * 60, x * 20 + 80, x * 3, x * 10);
 }
+
+*/
 
 function draw () {
 
@@ -20,4 +38,5 @@ var size =    Math.abs(mouseX - 320);
 
 ellipse(320, 240, mouseX, mouseY);
 
+}
 }
